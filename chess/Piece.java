cd4cdfee4c33444e21ad758b5b9731b6b6c71ace
Habeen-Jun <junhabin@gmail.com>
2024-02-17 22:02:14 -0500

    add: basic check and checkmate setup

--- chess/Piece.java
+++ chess/Piece.java
@@ -7,10 +7,22 @@
 
 package chess;
 import chess.ReturnPiece.PieceFile;
+import java.util.*;
 
 public interface Piece
 {
 
 
     String getPosition();
+
+    String getStringOfPosition(PieceFile file, int rank);
+
+    Map<String, ReturnPiece> populateRegularAndKillMoves();
+
+    public default boolean isValidMove(PieceFile newFile, int newRank) {
+        // check if the move is valid for the piece
+        // return true if the move is valid, false otherwise
+        Map<String, ReturnPiece> moves = this.populateRegularAndKillMoves();
+        return moves.containsKey(getStringOfPosition(newFile, newRank));
+    }
 }
